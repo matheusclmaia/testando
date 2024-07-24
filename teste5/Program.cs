310ff6bb7fc50c88c6efbfc0163f4ff9adf9a3a3
Matheus <Matheus@DESKTOP-U45IB5V>
2024-07-24 19:05:25 -0300

    Add project files.

---
+++ teste5/Program.cs
@@ -0,0 +1,11 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        string nome = "joão";
+        Console.WriteLine("Hello, World!     "+ nome);
+        Console.ReadKey();
+    }
+}
